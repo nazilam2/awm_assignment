a5eceb04bd75cbfcdb419e123e43d69f8ca74efb
Nazila Malekzadah <Nazilamalekzadah4@gmail.com>
2025-01-01 16:19:16 +0000

    Updated requirements.txt for Django 5.1.2 compatibility

--- awmca2/settings.py
+++ awmca2/settings.py
@@ -320,6 +320,9 @@ import os
 #GDAL_LIBRARY_PATH = r'C:\Users\Nazil\anaconda3\envs\awm_env\Library\bin\gdal.dll'
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH', r'C:\Users\Nazil\anaconda3\envs\awm_env\Library\bin\gdal.dll')
 
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH', '/app/.heroku/vendor/lib/libgdal.so')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH', '/app/.heroku/vendor/lib/libgeos_c.so')
+
 
 
 CORS_ORIGIN_ALLOW_ALL = False
